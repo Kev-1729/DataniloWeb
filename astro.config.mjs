fd175aab032e360b33ba960538289b6968ff6bf6
Kevin Tupac Agüero <kevin.tupac@unmsm.com>
2024-09-12 17:31:33 -0500

    Actualizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel'; // Importa el adaptador para Vercel
 
 export default defineConfig({
-  output: 'server', // Esto es importante para usar Vercel como servidor
-  adapter: vercel(), // Configura el adaptador de Vercel
 });
